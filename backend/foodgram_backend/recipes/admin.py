648bacae750f269d5c5ab288178381bdd8d2f4b8
Faburlas <44576572+bogomolovss@users.noreply.github.com>
2023-07-18 17:47:23 +0300

    fix admin files

--- backend/foodgram_backend/recipes/admin.py
+++ backend/foodgram_backend/recipes/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 
-from recipes.models import Recipe
+from recipes.models import Favorite, Follow, Recipe, ShoppingCart
 
-admin.site.register(Recipe)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'count_favorites')
+    list_filter = ('author', 'name', 'tags')
+
+    def count_favorites(self, obj):
+        return obj.favorites.count()
+
+
+admin.site.register(ShoppingCart)
+admin.site.register(Favorite)
+admin.site.register(Follow)
+admin.site.register(Recipe, RecipeAdmin)
