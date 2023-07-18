62c9bbcaea42a7992ad52cc206f436334376258e
Faburlas <44576572+bogomolovss@users.noreply.github.com>
2023-07-18 16:18:21 +0300

    Update admin.py

--- backend/foodgram_backend/users/admin.py
+++ backend/foodgram_backend/users/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from users.models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name')
+    list_filter = ('email', 'username')
+
+
+admin.site.register(User, UserAdmin)
